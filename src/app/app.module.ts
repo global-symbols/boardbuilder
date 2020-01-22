d9ba52b58eae782dd74e2713667a529080402fdf
Russell Newman <rn@russellnewman.co.uk>
2020-01-22 15:44:41 +0000

    Hide-able sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BuilderComponent } from './builder/builder.component';
+import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuilderComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule
   ],
+  // entryComponents: [BuilderComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
