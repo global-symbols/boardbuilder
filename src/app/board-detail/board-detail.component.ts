3b6d7ddb3482f6275b7eec88099db4501a0fbebf
Russell Newman <rn@russellnewman.co.uk>
2020-03-03 14:26:40 +0000

    Switch to scss, style BoardDetail

--- src/app/board-detail/board-detail.component.ts
+++ src/app/board-detail/board-detail.component.ts
@@ -5,7 +5,7 @@ import {Cell} from '../models/cell.model';
 @Component({
   selector: 'app-board-detail',
   templateUrl: './board-detail.component.html',
-  styleUrls: ['./board-detail.component.css']
+  styleUrls: ['./board-detail.component.scss']
 })
 export class BoardDetailComponent implements OnInit {
 
