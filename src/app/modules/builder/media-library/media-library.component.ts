d4d25e0cf69bdc4e60d299c80675b86f0a43b6b8
Russell Newman <rn@russellnewman.co.uk>
2020-09-10 17:54:57 +0100

    Dropzone, improve file validation

--- src/app/modules/builder/media-library/media-library.component.ts
+++ src/app/modules/builder/media-library/media-library.component.ts
@@ -2,6 +2,13 @@ import {Component, ElementRef, OnInit, Output, ViewChild, EventEmitter} from '@a
 import {Media} from '@data/models/media.model';
 import {MediaService} from '@data/services/media.service';
 
+
+export enum UploadStatus {
+  Idle = 'Idle',
+  Uploading = 'Uploading',
+  Error = 'Error'
+}
+
 @Component({
   selector: 'app-media-library',
   templateUrl: './media-library.component.html',
@@ -12,8 +19,11 @@ export class MediaLibraryComponent implements OnInit {
   loadingMedia = false;
   media: Media[];
 
-  uploadingMedia = false;
-  uploadingMediaError = false;
+  uploadStatus: UploadStatus;
+
+  dropzoneActive = false;
+
+  allowedTypes = ['image/svg+xml', 'image/jpeg', 'image/gif', 'image/png'];
 
   @Output() readonly mediaSelect = new EventEmitter<Media>();
 
@@ -22,8 +32,9 @@ export class MediaLibraryComponent implements OnInit {
   constructor(private service: MediaService) { }
 
   ngOnInit(): void {
-    // TODO: load media when panel becomes visible
+    // TODO: lazy-load media when panel becomes visible
     this.loadMedia();
+    this.uploadStatus = UploadStatus.Idle;
   }
 
   loadMedia(): void {
@@ -34,31 +45,47 @@ export class MediaLibraryComponent implements OnInit {
     });
   }
 
-  selectFile() {
-    this.uploadingMediaError = false;
+  openFileSelector() {
+    this.uploadStatus = UploadStatus.Idle;
     this.fileUpload.nativeElement.click();
   }
 
-  uploadFile() {
-    this.uploadingMediaError = false;
+  selectFile() {
     if (this.fileUpload.nativeElement.files.length === 1) {
-      this.uploadingMedia = true;
-
-      this.service.add(this.fileUpload.nativeElement.files[0]).subscribe(media => {
-        this.media.push(media);
-        this.mediaSelect.emit(media);
-
-        // Clear the <input> value and reset uploading status
-        this.fileUpload.nativeElement.value = null;
-        this.uploadingMedia = false;
-      },
-      error => {
-        this.uploadingMediaError = true;
-
-        // Clear the <input> value and reset uploading status
-        this.fileUpload.nativeElement.value = null;
-        this.uploadingMedia = false;
-      });
+      this.uploadFile(this.fileUpload.nativeElement.files[0]);
     }
+  }
+
+  dropFile($event: DragEvent) {
+    this.allowDragDrop($event);
+    const files = $event.dataTransfer.files;
+    if (files.length === 1) { this.uploadFile(files[0]); }
+  }
+
+  uploadFile(file: File) {
+    if (this.allowedTypes.includes(file.type)) {
+      this.uploadStatus = UploadStatus.Uploading;
+
+      this.service.add(file).subscribe(media => {
+          this.media.push(media);
+          this.mediaSelect.emit(media);
+
+          // Clear the <input> value and reset uploading status
+          this.fileUpload.nativeElement.value = null;
+          this.uploadStatus = UploadStatus.Idle;
+        },
+        error => {
+          // Clear the <input> value and reset uploading status
+          this.fileUpload.nativeElement.value = null;
+          this.uploadStatus = UploadStatus.Error;
+        });
+    } else {
+      this.uploadStatus = UploadStatus.Error;
+    }
+  }
+
+  allowDragDrop($event: DragEvent) {
+    $event.preventDefault();
+    $event.stopPropagation();
   }
 }
