d758452f0ea42d4a1dcde05d0d91e07080674ebf
Russell Newman <rn@russellnewman.co.uk>
2021-01-16 02:21:11 +0000

    Board SVG Preview: working grid

--- src/app/shared/components/board-preview-svg/board-preview-svg.component.ts
+++ src/app/shared/components/board-preview-svg/board-preview-svg.component.ts
@@ -1,5 +1,12 @@
-import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 import {Board} from '@data/models/board.model';
+
+interface BoardPreviewSvgThumbnail {
+  height: number;
+  width: number;
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-board-preview-svg',
@@ -17,7 +24,20 @@ export class BoardPreviewSvgComponent implements OnChanges {
 
   height: number;
   width: number;
+
+  viewBox: string;
+
   pageOutlineColour = '#6b6b6b';
+  pageOutlineWidth: number;
+
+  symbolSize = 24;
+
+  pagePadding = 0;
+
+  pageInnerHeight: number;
+  pageInnerWidth: number;
+
+  thumbnails: BoardPreviewSvgThumbnail[];
 
   constructor() { }
 
@@ -29,6 +49,34 @@ export class BoardPreviewSvgComponent implements OnChanges {
       this.height = this.a4.shortEdge;
       this.width = this.a4.longEdge;
     }
+
+    this.viewBox = `0 0 ${this.width} ${this.height}`;
+
+    this.pageOutlineWidth = this.height / 26.25;
+
+    this.pageInnerHeight = this.height - this.pagePadding;
+    this.pageInnerWidth = this.width - this.pagePadding;
+
+    this.thumbnails = [];
+
+    for (let row = 1; row <= this.board.rows; row++) {
+
+      const cellY = this.pageInnerHeight / this.board.rows * (row - 1);
+
+      for (let col = 1; col <= this.board.columns; col++) {
+
+        const cellX = this.pageInnerWidth / this.board.columns * (col - 1);
+
+        this.thumbnails.push({
+          x: cellX,
+          y: cellY,
+          height: this.pageInnerHeight / this.board.rows,
+          width: this.pageInnerWidth / this.board.columns,
+        });
+
+      }
+    }
+    console.log(this);
   }
 
 }
