79b2386c807d3a3b3a65af73ef2a6530d2e4467e
Russell Newman <rn@russellnewman.co.uk>
2021-01-18 11:42:28 +0000

    Improve colour pickers

--- src/app/shared/components/colour-selector/colour-selector.component.ts
+++ src/app/shared/components/colour-selector/colour-selector.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 import {COLOURS} from '../../../colour-palette';
 
 @Component({
@@ -6,18 +6,14 @@ import {COLOURS} from '../../../colour-palette';
   templateUrl: './colour-selector.component.html',
   styleUrls: ['./colour-selector.component.scss']
 })
-export class ColourSelectorComponent implements OnInit {
+export class ColourSelectorComponent {
 
   colours = COLOURS;
 
+  @Input() icon = 'color_lens';
   @Input() label;
   @Input() value;
   @Output() valueChange = new EventEmitter<string>();
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   selectColour(colour: string) {
     this.valueChange.emit(colour);
