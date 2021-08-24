a85f06ed2ed12955d935d3d63000c6ecbc21328a
Russell Newman <rn@russellnewman.co.uk>
2021-08-24 19:58:29 +0100

    Add new locales, fix interpolation errors in XLF files

--- src/app/data/services/locale.service.ts
+++ src/app/data/services/locale.service.ts
@@ -23,9 +23,24 @@ export class LocaleService {
 
   availableLocales(): Locale[] {
     return [
-      { name: 'English', code: 'en', iso639_1: 'en' },
-      { name: 'French', code: 'fr', iso639_1: 'fr' },
-    ];
+      { code: 'en', iso639_1: 'en', name: 'English' },
+      { code: 'sq', iso639_1: 'fr', name: 'Albanian' },
+      { code: 'ar', iso639_1: 'fr', name: 'Arabic' },
+      { code: 'hy', iso639_1: 'fr', name: 'Armenian' },
+      { code: 'bg', iso639_1: 'fr', name: 'Bulgarian' },
+      { code: 'ca', iso639_1: 'fr', name: 'Catalan' },
+      { code: 'hr', iso639_1: 'fr', name: 'Croatian' },
+      { code: 'nl', iso639_1: 'fr', name: 'Dutch' },
+      { code: 'fr', iso639_1: 'fr', name: 'French' },
+      { code: 'de', iso639_1: 'fr', name: 'German' },
+      { code: 'el', iso639_1: 'fr', name: 'Greek' },
+      { code: 'ro', iso639_1: 'fr', name: 'Romanian' },
+      { code: 'sr-cy', iso639_1: 'sr', name: 'Serbian (Cyrillic)' },
+      { code: 'sr-la', iso639_1: 'sr', name: 'Serbian (Latin)' },
+      { code: 'es', iso639_1: 'es', name: 'Spanish' },
+      { code: 'tr', iso639_1: 'tr', name: 'Turkish' },
+      { code: 'ur-pk', iso639_1: 'ur', name: 'Urdu (Pakistan)' },
+    ].sort((a, b) => a.name < b.name ? -1 : 1);
   }
 
   changeLocale(code: string): void {
