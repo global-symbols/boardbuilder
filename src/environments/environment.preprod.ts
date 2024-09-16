d7e6eef8748573b8dd06aabada5e87398e80d697
Richard Stuart <richard.k.stuart@gmail.com>
2024-09-16 11:04:59 +0100

    Add pre-prod environment, GitHub Actions workflows, and update angular.json

---
+++ src/environments/environment.preprod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+
+  globalSymbolsBase: 'https://app.gs-test.co.uk',
+
+  globalSymbolsApiBase: 'https://app.gs-test.co.uk/api/v1',
+  boardBuilderApiBase: 'https://app.gs-test.co.uk/api/boardbuilder/v1',
+
+  globalSymbolsOauthClientId: 'RKBryis4wFurzoH14-J9gMiHbXHRll6YHTZXrYVJc_A'
+};
